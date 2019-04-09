cec817a7d6fa3ae7be234f6929a4d09fe85002a9
screenfilleddesk@gmail.com <screenfilleddesk@gmail.com>
2019-04-09 13:16:42 +0200

    Fixing some stuff, adding Prosjekt

---
+++ Oblig1_JPA/src/no/hvl/dat107/Prosjekt.java
@@ -0,0 +1,13 @@
+package no.hvl.dat107;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "oblig1_jpa", name = "prosjekt")
+public class Prosjekt {
+
+}
---
+++ Oblig1_JPA/src/no/hvl/dat107/ProsjektEAO.java
@@ -0,0 +1,26 @@
+package no.hvl.dat107;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class ProsjektEAO {
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("ovingPersistenceUnit");
+	
+	public Prosjekt finnAvdelingMedId(int prosjektId) {
+
+		EntityManager em = emf.createEntityManager();
+
+		Prosjekt prosjekt;
+
+		try {
+			prosjekt = em.find(Avdeling.class, prosjektId);
+
+		} finally {
+			em.close();
+		}
+		return prosjekt;
+	}
+}
